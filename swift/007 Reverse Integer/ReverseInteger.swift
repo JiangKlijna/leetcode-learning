6f5fdf11cc9f43a80f11e80cdb980d3ee9f72d7f
jiangKlijna <leil729564090@outlook.com>
2016-12-29 20:49:07 +0800

    :fire:Reverse Integer

--- swift/007 Reverse Integer/ReverseInteger.swift
+++ swift/007 Reverse Integer/ReverseInteger.swift
@@ -1,19 +1,15 @@
 
 // 本题题意：给出一个int，反轉整形數
-
 class Solution {
     func reverse(_ x: Int) -> Int {
-        var re:Int = 0
-        var maxint:Int = 2147483647
-        var y:Int = x < 0 ? -x : x
-
+        var re = 0, maxint = 2147483647, y = x < 0 ? -x : x
         while y > 0 {
-          if re != 0 && maxint / re < 10{
-            return 0
-          }
-          re *= 10
-          re += y%10
-          y /= 10
+            if re != 0 && maxint / re < 10{
+                return 0
+            }
+            re *= 10
+            re += y%10
+            y /= 10
         }
         return x < 0 ? -re : re
     }
