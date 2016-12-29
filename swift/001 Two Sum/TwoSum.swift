6de8f48dba3222e24773c35c3069f6180d533019
jiangKlijna <leil729564090@outlook.com>
2016-12-29 20:48:27 +0800

    :fire:Two Sum

--- swift/001 Two Sum/TwoSum.swift
+++ swift/001 Two Sum/TwoSum.swift
@@ -3,7 +3,6 @@
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution.
  * 给出一个int数组和目标值,从数组中找到两个值使得其和为目标值.返回两个下标位置.
- *
  */
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
