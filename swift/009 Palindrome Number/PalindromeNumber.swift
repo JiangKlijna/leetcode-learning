8356bfdeaa8cc28ee3c195735cc97edbb2ed25a4
jiangKlijna <leil729564090@outlook.com>
2016-12-29 20:49:59 +0800

    :fire:Palindrome Number

--- swift/009 Palindrome Number/PalindromeNumber.swift
+++ swift/009 Palindrome Number/PalindromeNumber.swift
@@ -1,16 +1,12 @@
 
-
 // 本题题意：判斷回文數字
-
 class Solution {
     func isPalindrome(_ x: Int) -> Bool {
-        let str:String = String(x)
-        let length:Int = str.characters.count
-        //let n:Int = length / 2
+        let str = String(x), length = str.characters.count
         for i in 0...(length / 2) {
-          if str[str.index(str.startIndex, offsetBy: i)] != str[str.index(str.startIndex, offsetBy: (length - 1 - i))] {
-            return false
-          }
+            if str[str.index(str.startIndex, offsetBy: i)] != str[str.index(str.startIndex, offsetBy: (length - 1 - i))] {
+                return false
+            }
         }
         return true
     }
