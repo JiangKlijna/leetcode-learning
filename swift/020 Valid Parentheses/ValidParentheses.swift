31f35609f89edca38859340422ce102883def16d
jiangKlijna <leil729564090@outlook.com>
2016-12-29 20:50:19 +0800

    :fire:Valid Parentheses

--- swift/020 Valid Parentheses/ValidParentheses.swift
+++ swift/020 Valid Parentheses/ValidParentheses.swift
@@ -1,17 +1,14 @@
 
-
 /**
   * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
   * The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
   */
 class Solution {
     func isValid(_ s: String) -> Bool {
-        var stack:[Int8] = []
-        let t1:Int8 = Int8(("}" as UnicodeScalar).value)
-        let t2:Int8 = Int8((")" as UnicodeScalar).value)
-        let t3:Int8 = Int8(("]" as UnicodeScalar).value)
+        var stack:[Int] = []
+        let t1 = Int(("}" as UnicodeScalar).value), t2 = Int((")" as UnicodeScalar).value), t3 = Int(("]" as UnicodeScalar).value)
         for cs in s.unicodeScalars {
-            let c = Int8(c.value)
+            let c = Int(cs.value)
             if c == t1 || c == t2 || c == t3 {
                 let length:Int = stack.count
                 if length == 0 {
