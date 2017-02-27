57d531133d6ebe8b2adbbeeb1af39d424cd8c713
jiangKlijna <leil729564090@outlook.com>
2017-02-27 13:48:59 +0800

    :fire:Remove Element

---
+++ swift/027 Remove Element/RemoveElement.swift
@@ -0,0 +1,18 @@
+
+/**
+ * Given an array and a value, remove all instances of that value in place and return the new length.
+ * Do not allocate extra space for another array, you must do this in place with constant memory.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ */
+class Solution {
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        var non_val_size = 0
+        for i in nums {
+            if i != val {
+                nums[non_val_size] = i
+                non_val_size += 1
+            }
+        }
+        return non_val_size
+    }
+}
