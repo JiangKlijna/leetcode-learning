bc42546b8b084277cf87e7a2ffd236a84041abb6
jiangKlijna <leil729564090@outlook.com>
2017-03-01 08:53:41 +0800

    :fire:Search Insert Position

---
+++ java/035 Search Insert Position/SearchInsertPosition.java
@@ -0,0 +1,18 @@
+
+//Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return left;
+    }
+}
