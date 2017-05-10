4d038855b8827789274065696c54f85ba96c610d
jiangKlijna <leil729564090@outlook.com>
2017-05-10 21:20:01 +0800

    :fire:Remove Duplicates from Sorted List

---
+++ go/083 Remove Duplicates from Sorted List/RemoveDuplicatesfromSortedList.go
@@ -0,0 +1,17 @@
+
+/**
+ * Given a sorted linked list, delete all duplicates such that each element appear only once.
+ * For example,
+ * Given 1->1->2, return 1->2.
+ * Given 1->1->2->3->3, return 1->2->3.
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {return head}
+    head.Next = deleteDuplicates(head.Next)
+    if head.Val == head.Next.Val {return head.Next} else {return head}
+}
