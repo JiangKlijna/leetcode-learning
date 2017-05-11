7cff72e851c1009396c7c556e17b87f41f4e2c1c
jiangKlijna <leil729564090@outlook.com>
2017-05-11 22:00:27 +0800

    :fire:Remove Duplicates from Sorted List

---
+++ js/083 Remove Duplicates from Sorted List/RemoveDuplicatesfromSortedList.js
@@ -0,0 +1,22 @@
+
+/**
+ * Given a sorted linked list, delete all duplicates such that each element appear only once.
+ * For example,
+ * Given 1->1->2, return 1->2.
+ * Given 1->1->2->3->3, return 1->2->3.
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ /**
+  * @param {ListNode} head
+  * @return {ListNode}
+  */
+var deleteDuplicates = function(head) {
+    if (head == null || head.next == null) return head;
+    head.next = deleteDuplicates(head.next);
+    return head.val == head.next.val ? head.next : head;
+};
