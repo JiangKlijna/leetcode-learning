35ee2e37db4f80e3841ba4d4bbf5edac9c6c6b00
jiangKlijna <leil729564090@outlook.com>
2017-05-17 17:03:10 +0800

    :fire:Missing Number

---
+++ py/268 Missing Number/MissingNumber.py
@@ -0,0 +1,11 @@
+
+# Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+# For example, Given nums = [0, 1, 3] return 2.
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #return len(nums) * (len(nums) + 1) / 2 - sum(nums)
+        return reduce(operator.xor, nums + range(len(nums) + 1))
